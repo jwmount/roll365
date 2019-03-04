448bb2cc10c5d3559172d18fa2674ad9b8a3b2e6
John W. Mount <john.w.mount@iCloud.com>
2019-03-04 10:23:00 -0800

    Equipment and Jobs both defined to ActiveAdmin.

--- app/admin/equipment.rb
+++ app/admin/equipment.rb
@@ -1,7 +1,8 @@
-ActiveAdmin.register Person do
+ActiveAdmin.register Equipment do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-  permit_params :first_name, :last_name, :title, :available, :available_on, :OK_to_contact, :active
+  permit_params :name
+
 
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,18 @@
 class Person < ApplicationRecord
+
+# Defaults
+=begin
+   "last_name"=>"Cuellar",
+   "title"=>"Manager",
+   "available"=>"1",
+   "available_on(1i)"=>"2019",
+   "available_on(2i)"=>"3",
+   "available_on(3i)"=>"4",
+   "available_on(4i)"=>"01",
+   "available_on(5i)"=>"24",
+   "OK_to_contact"=>"1",
+   "active"=>"1"},
+ "commit"=>"Update Person",
+=end
+
 end
