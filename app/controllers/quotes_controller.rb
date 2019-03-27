d22159b6d9a93f6bfaf83ed0e7bfa8c6fbc79bc3
John W. Mount <john.w.mount@iCloud.com>
2019-03-27 09:45:14 -0700

    Removed length from params list in quotes_controller.rb.  Why does this exist?

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,7 +3,7 @@ class QuotesController < InheritedResources::Base
   private
 
     def quote_params
-      params.require(:quote).permit(:project_id, :quote_to_id, :rep_id, :name{30}, :fire_ants, :fire_ants_verified_by, :inclusions, :expected_start, :duration, :council)
+      params.require(:quote).permit(:project_id, :quote_to_id, :rep_id, :name, :fire_ants, :fire_ants_verified_by, :inclusions, :expected_start, :duration, :council)
     end
 end
 
