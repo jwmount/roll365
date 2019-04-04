e75901854aaa88041621a14fed380eb14ff7ce95
John W. Mount <john.w.mount@iCloud.com>
2019-04-04 14:59:16 -0700

    Addressables list works for Company, Person and Project. This can become a rollodex service in time.

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,5 +1,30 @@
 class AddressesController < InheritedResources::Base
 
+ 
+   
+  def index
+ 
+    @addresses = Address.corporate
+    @projects = Address.project
+    @people = Address.person
+
+    @addresses.each do |addr|
+     	@company_name = Company.find(addr.addressable_id).name
+        addr.addressable_type = @company_name
+    end
+
+    @projects.each do |addr|
+      @proj_name = Project.find(addr.addressable_id).name
+      addr.addressable_type = @proj_name
+    end
+
+    @people.each do |addr|
+      @person_name = People.find(addr.addressable_id).name
+      addr.addressable_type = @person_name
+    end
+
+  end
+
   private
 
     def address_params
--- app/models/address.rb
+++ app/models/address.rb
@@ -3,4 +3,8 @@ class Address < ApplicationRecord
 
   belongs_to :addressable, polymorphic: true
 
+  scope :corporate,      -> { where(addressable_type: 'Company') }
+  scope :project,        -> { where(addressable_type: 'Project') }
+  scope :person,         -> { where(addressable_type: 'Person') }
+
 end
