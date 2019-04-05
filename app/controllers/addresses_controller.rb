060d497359568eba041bd53ce6333598eeebd15b
John W. Mount <john.w.mount@iCloud.com>
2019-04-05 10:32:14 -0700

    Address operations index, edit and show display more info on who or what parent is. Needs to be improved but not urgently.

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,34 +1,22 @@
 class AddressesController < InheritedResources::Base
 
- 
-   
+
+  # 
+  # Prepend parent type to each address
+  #  
   def index
- 
-    @addresses = Address.corporate
-    @projects = Address.project
-    @people = Address.person
-
+    @addresses = Address.all
     @addresses.each do |addr|
-     	@company_name = Company.find(addr.addressable_id).name
-        addr.addressable_type = @company_name
+      addr.addressable_type += ':  ' + Company.find(addr.addressable_id).name
     end
-
-    @projects.each do |addr|
-      @proj_name = Project.find(addr.addressable_id).name
-      addr.addressable_type = @proj_name
-    end
-
-    @people.each do |addr|
-      @person_name = People.find(addr.addressable_id).name
-      addr.addressable_type = @person_name
-    end
-
   end
+
 
   private
 
     def address_params
       params.require(:address).permit(:addressable_id, :addressable_type, :street_address, :city, :state, :post_code, :map_reference, :longitude, :latitude)
     end
+
 end
 
--- app/models/address.rb
+++ app/models/address.rb
@@ -3,8 +3,7 @@ class Address < ApplicationRecord
 
   belongs_to :addressable, polymorphic: true
 
-  scope :corporate,      -> { where(addressable_type: 'Company') }
-  scope :project,        -> { where(addressable_type: 'Project') }
-  scope :person,         -> { where(addressable_type: 'Person') }
+  
+  
 
 end
