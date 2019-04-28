d4401c814f4844a6968733bf6c60679ab9700580
John W. Mount <john.w.mount@iCloud.com>
2019-04-27 17:24:58 -0700

    Bootstrapped forms for Company and Address.

--- app/models/company.rb
+++ app/models/company.rb
@@ -62,7 +62,7 @@ class Company < ApplicationRecord
                       }
   end
 
-  enum genre: { fantasy: 1, romance: 2, horror: 3, fiction: 4, poetry: 5 }
+  enum credit: { no: 1, week: 2, fortnight: 3, month: 4, open: 5 }
   #
   # S C O P E S
   #
