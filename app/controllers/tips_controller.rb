48a2f55dfbe6c485140c9e47ade0c011da2b797b
John W. Mount <john.w.mount@iCloud.com>
2019-05-14 13:03:02 -0700

    Tip sites given the Finder layout and functions. Should probably take advatage of Addresses and Identifiers, but don't now.

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,6 +1,60 @@
 class TipsController  < ApplicationController # < InheritedResources::Base< InheritedResources::Base
 
+  before_action :set_tip, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @q = Tip.ransack(params[:q])
+    @tips = @q.result.order(name: 'ASC').paginate(page: params[:page], per_page: 10 || params[:per_page])
+    flash[:Information] = "Tip sites are usually land fills or disposal facilities.  Fire ants are an environmental contaminant that you can't take to specified locations."
+  end
+
+  def show
+  	@tip = set_tip
+  end
+
+  def edit
+  	@tip = set_tip
+  end
+
+  def new
+  	@tip = Tip.new
+  end
+  	
+  # POST /companies
+  # POST /companies.json
+  def create
+    @tip = Tip.new(tip_params)
+    respond_to do |format|
+      if @tip.save
+        format.html { redirect_to @tip, notice: 'Tip was successfully created.' }
+        format.json { render :show, status: :created, location: @tip }
+      else
+        format.html { render :new }
+        format.json { render json: @tip.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /companies/1
+  # PATCH/PUT /companies/1.json
+  def update
+    respond_to do |format|
+      if @tip.update!(tip_params)
+        format.html { redirect_to @tip, notice: "{@tip} has been updated." }
+        format.json { render :show, status: :ok, location: @tip }
+      else
+        format.html { render :edit }
+        format.json { render json: @tip.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_tip
+      @tip = Tip.find(params[:id])
+    end
 
     def tip_params
       params.require(:tip).permit(:name, :company_id, :fee, :fire_ant_risk_level)
--- app/models/tip.rb
+++ app/models/tip.rb
@@ -1,2 +1,7 @@
 class Tip < ApplicationRecord
+
+  def display_address
+  	'No address'
+  end
+  
 end
