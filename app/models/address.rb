5b64fe361f8aac41061d1a526996b4da59c16937
John W. Mount <john.w.mount@iCloud.com>
2019-05-18 10:00:43 -0700

    Addresses#index 505 on addressable_type = 'People' fixed by adding def display name to address.rb.

--- app/models/address.rb
+++ app/models/address.rb
@@ -24,6 +24,14 @@ class Address < ApplicationRecord
   end
 
   #
+  # Our Address model is polymorphic to resources with names we want to display as addressable members.
+  # Companies have .name attributes, People ... Addresses have neither.  
+  # Why doesn't matter what this name is?  Not clear!
+  #
+  def display_name
+    'Ralph'
+  end
+#
 # Get and display parent name of polymorphic Address. [TODO]: remove after May 1
 #
   def DEPRECATEDdisplay_parent_name 
