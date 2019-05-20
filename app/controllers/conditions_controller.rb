0c7aadb70f8f38dc225bd6dbba9a4faf0ebbff0b
John W. Mount <john.w.mount@iCloud.com>
2019-05-19 20:44:53 -0700

    Conditions resource Built out.  Mostly working, but not marked up.

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -1,8 +1,33 @@
-class ConditionsController  < ApplicationController # < InheritedResources::Base< InheritedResources::Base
+class ConditionsController  < ApplicationController
 
+  
   def index
-  	render inline: "Hang on there while the actions get defined, apparently they were all in ActiveAdmin"
+    @q = Condition.ransack(params[:q])
+    @conditions = @q.result.order(name: 'ASC').paginate(page: params[:page], per_page: 10 || params[:per_page])
+    flash[:info] = "Hang on there while the actions get defined, apparently they were all in ActiveAdmin"
   end
+
+  def new
+    @condition = Condition.new
+  end
+
+  def create
+  	@condition = Condition.new(condition_params)
+  	respond_to do |format|
+      if @condition.save
+        format.html { redirect_to @condition, notice: 'Condition was successfully created.' }
+        format.json { render :show, status: :created, location: @condition }
+      else
+        format.html { render :new }
+        format.json { render json: @condition.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def show
+  	@condition = Condition.find(params[:id])
+  end
+  	
 
   private
 
