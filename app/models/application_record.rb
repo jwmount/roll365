6f6b7cf6964e686acadd2145de19810458f17ef7
John W. Mount <john.w.mount@iCloud.com>
2019-08-07 13:08:18 -0700

    deleted:    app/models/application_record.rb

--- app/models/application_record.rb
+++
@@ -1,7 +0,0 @@
-# app/models/application_record.rb
-# the presence of this file prevents rails migrations from recreating application_record.rb
-# see https://github.com/rails/rails/issues/29407
-
-#REMOVErequire 'models/application_record.rb'
-
-
