f9544ce7d112961a1e0ab0cf196bdb6240b0b2f0
John W. Mount <john.w.mount@iCloud.com>
2019-08-07 12:29:42 -0700

    Remove app/models/application_record.rb as it causes the fail on Heroku and is outdated.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,6 +2,6 @@
 # the presence of this file prevents rails migrations from recreating application_record.rb
 # see https://github.com/rails/rails/issues/29407
 
-require 'models/application_record.rb'
+#REMOVErequire 'models/application_record.rb'
 
 
