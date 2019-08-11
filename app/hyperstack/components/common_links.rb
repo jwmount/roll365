b609d0090285b663b791574ab266b77b04269807
John W. Mount <john.w.mount@iCloud.com>
2019-08-11 12:37:54 -0700

    Serving Suggestion: Common Services and Messages ticker, suggestion.

--- app/hyperstack/components/common_links.rb
+++ app/hyperstack/components/common_links.rb
@@ -2,16 +2,18 @@ class CommonLinks < HyperComponent
   include Hyperstack::Router::Helpers
 
   render (DIV) do
-    
-    H4 {"Common-Links Component -- future features?"}
-    
-    A(href: '/') { 'Home' }
-     
+
+    H4 {"Common Services Component -- future features?"}
+
+
     #A(href: "http://localhost:5000/") {'Home 2'} works fine
-    DIV do
-      A(href: "http://localhost:5000/") {'Home 2'}
+    DIV(class: 'roll365-app') do
+      A(href: '/') { '  Home  ' }
+      A(href: "http://localhost:5000/") {'  Home 2  '}
+      P
+      BODY { '10:11:15: Departed inspection station at Ashland Northbound.' }
     end
 
   end
-    
+
 end
