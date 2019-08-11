60cd8a811aec25ad8e6ad8ed906d4ad885cac54c
John W. Mount <john.w.mount@iCloud.com>
2019-08-11 10:34:03 -0700

    Route using dashboard :*...', mounts: Dashboard in App, seems to work as shipment_index is display, prssume because now mounted.

--- app/hyperstack/components/app.rb
+++ app/hyperstack/components/app.rb
@@ -6,7 +6,8 @@ class App < HyperComponent
     Header()
     Route('/', exact: true) {Redirect('dashboard/all') }
     Route('/dashboard', exact: true) { Redirect('/dashboard/all') }
-#    Route('/dashboard/:scope', mounts: Dashboard)   # scopes are :ontime, :delayed, :completed  was Index
+    Route('/dashboard/:*...', mounts: Dashboard)
+    Route('/dashboard/:scope', mounts: Dashboard)   # scopes are :ontime, :delayed, :completed  was Index
     Footer()
   end
 end
