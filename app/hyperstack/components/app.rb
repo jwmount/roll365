642985cbd356b65e91e57cd73d4633fdc7d29bef
John W. Mount <john.w.mount@iCloud.com>
2019-08-11 18:27:23 -0700

    Added Hyperstack::Component and Hyperstack::Router::Helpers to app.  WOrks, lag between Rails and HS is still too ong.

--- app/hyperstack/components/app.rb
+++ app/hyperstack/components/app.rb
@@ -7,12 +7,12 @@ class App < HyperComponent
 #  render(SECTION, class: 'roll365-app' ) do
   render do
     Header()
-    Route('/', exact: true) {Redirect('dashboard/all') }
-    Route('/dashboard', exact: true) { Redirect('/dashboard/all') }
-
+    Route('/', exact: true) mounts: Dashboard ) #{Redirect('dashboard/all') }
+  #  Route('/dashboard', exact: true) { Redirect('/dashboard/all') }
+    Route('/dashboard', mounts: Dasboard)
     # Unclear if both of these next two are useful?
-    Route('/dashboard/:*...', mounts: Dashboard)
-    Route('/dashboard/:scope', mounts: Dashboard)   # scopes are :ontime, :delayed, :completed  was Index
+  #  Route('/dashboard/:*...', mounts: Dashboard)
+  #  Route('/dashboard/:scope', mounts: Dashboard)   # scopes are :ontime, :delayed, :completed  was Index
 
     Footer()
   end
