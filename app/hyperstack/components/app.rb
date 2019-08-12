2280c4376e738e23d1c070375074f6b1d7283929
John W. Mount <john.w.mount@iCloud.com>
2019-08-11 19:09:37 -0700

    Reverted app to original routes.

--- app/hyperstack/components/app.rb
+++ app/hyperstack/components/app.rb
@@ -1,18 +1,16 @@
 
 class App < HyperComponent
   include Hyperstack::Component
-  include Hyperstack::Router::Helpers
   include Hyperstack::Router
 
 #  render(SECTION, class: 'roll365-app' ) do
   render do
     Header()
-    Route('/', exact: true, mounts: Dashboard ) #{Redirect('dashboard/all') }
-  #  Route('/dashboard', exact: true) { Redirect('/dashboard/all') }
-    Route('/dashboard', mounts: Dashboard)
-    # Unclear if both of these next two are useful?
-  #  Route('/dashboard/:*...', mounts: Dashboard)
-  #  Route('/dashboard/:scope', mounts: Dashboard)   # scopes are :ontime, :delayed, :completed  was Index
+    Route('/', exact: true ) {Redirect('dashboard/all') }
+    Route('/dashboard', exact: true) { Redirect('/dashboard/all') }
+  # Unclear if both of these next two are useful?
+    Route('/dashboard/:*...', mounts: Dashboard)
+    Route('/dashboard/:scope', mounts: Dashboard)   # scopes are :ontime, :delayed, :completed  was Index
 
     Footer()
   end
